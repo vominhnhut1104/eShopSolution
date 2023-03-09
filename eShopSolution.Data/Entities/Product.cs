c5d395c91b92dfec7b23785b3888adf4ddf4c539
vominhnhut1104 <vominhnhut1104@gmail.com>
2023-03-09 10:19:58 +0700

    Thêm bảng hình ảnh sản phẩm

--- eShopSolution.Data/Entities/Product.cs
+++ eShopSolution.Data/Entities/Product.cs
@@ -26,5 +26,7 @@ namespace eShopSolution.Data.Entities
 
        public List<ProductTranslation> ProductTranslations { get; set; }
 
+        public List<ProductImage> ProductImages { get; set; }
+
     }
 }
