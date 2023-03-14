cdbe4c0e726491291b36164191bb24d5270c9419
vominhnhut1104 <vominhnhut1104@gmail.com>
2023-03-14 11:41:20 +0700

    update login

--- eShopSolution.Application/System/Users/IUserService.cs
+++ eShopSolution.Application/System/Users/IUserService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using eShopSolution.ViewModels.System.Users;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,11 @@ using System.Threading.Tasks;
 
 namespace eShopSolution.Application.System.Users
 {
-    internal interface IUserService
+    public interface IUserService
     {
+        Task<string> Authencate(LoginRequest request);
+
+        Task<bool> Register(RegisterRequest request);
+
     }
 }
--- eShopSolution.BackendApi/Program.cs
+++ eShopSolution.BackendApi/Program.cs
@@ -1,6 +1,10 @@
 using eShopSolution.Application.Catalog.Products;
+using eShopSolution.Application.Common;
+using eShopSolution.Application.System.Users;
 using eShopSolution.Data.EF;
+using eShopSolution.Data.Entities;
 using eShopSolution.Utilities.Constants;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.OpenApi.Models;
@@ -26,10 +30,24 @@ builder.Services.AddDbContext<EShopDbContext>(options =>
         });
 });
 
+builder.Services.AddIdentity<AppUser, AppRole>()
+               .AddEntityFrameworkStores<EShopDbContext>()
+               .AddDefaultTokenProviders();
+
 
 // declare DI
 
 builder.Services.AddTransient<IPublicProductService, PublicProductService>();
+builder.Services.AddTransient<IStorageService, FileStorageService>();
+
+builder.Services.AddTransient<IPublicProductService, PublicProductService>();
+builder.Services.AddTransient<IManageProductService, ManageProductService>();
+
+builder.Services.AddTransient<UserManager<AppUser>, UserManager<AppUser>>();
+builder.Services.AddTransient<SignInManager<AppUser>, SignInManager<AppUser>>();
+builder.Services.AddTransient<RoleManager<AppRole>, RoleManager<AppRole>>();
+builder.Services.AddTransient<IUserService, UserService>();
+
 
 builder.Services.AddSwaggerGen(c =>
 {
